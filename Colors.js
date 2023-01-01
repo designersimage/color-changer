6aa4c22f5ad12fd0e4ada958100267579d93fb64
Designer's Image <designersimage.io@gmail.com>
2022-12-31 18:12:09 -0800

    added color structure

---
+++ Colors.js
@@ -0,0 +1,55 @@
+export class RGB
+{
+    constructor() {
+
+    }
+}
+
+export class HEX
+{
+    constructor() {
+        
+    }
+}
+
+export class CMYK
+{
+    constructor() {
+        
+    }
+}
+
+export class HSL
+{
+    constructor() {
+        
+    }
+}
+
+export class HSV
+{
+    constructor() {
+        
+    }
+}
+
+export class HWB
+{
+    constructor() {
+        
+    }
+}
+
+export class NCOL
+{
+    constructor() {
+        
+    }
+}
+
+export class NAME
+{
+    constructor() {
+        
+    }
+}
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+/**
+ * A color conversion tool to convert between color types for web css and javascript.
+ * The available color conversion types are:
+ *  - hex
+ *  - rgb / rgba
+ *  - hsl / hsla
+ *  - hwb / hwba
+ *  - cmyk
+ *  - ncol
+ *  - HTML Name
+ *  - ?lab, ?ansi, ?ansi16, ?ansai256, ?hpl, ?xyz
+ * 
+ * @package color-changer
+ * @author Jonathan Wheeler <jonathan@desginersimage.io>
+ */
+import { RGB, HEX, CMYK, HSL, HSV, HWB, NCOL, NAME } from "Colors"
+
 class Color
 {
     constructor() {
-        this.rgb = {
-            r: 0,
-            g: 0,
-            b: 0
-        }
+        
     }
 }
 
